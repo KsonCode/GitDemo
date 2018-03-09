111ac40dbc53c840039bc061eb60944e2ceebd16
KsonCode <19655910@qq.com>
2018-03-09 11:59:01 +0800

    新版提交

--- app/src/main/java/com/example/kson/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/kson/gitdemo/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("======");
+
+        System.out.println("1212121212");
     }
 }
---
+++ app/src/main/java/com/example/kson/gitdemo/Test1.java
@@ -0,0 +1,10 @@
+package com.example.kson.gitdemo;
+
+/**
+ * Author:kson
+ * E-mail:19655910@qq.com
+ * Time:2018/03/09
+ * Description:
+ */
+public class Test1 {
+}
